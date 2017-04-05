4c2c910894831bed3c294877c7c0637d8c7c1527
Ray <leixx086@umn.edu>
2017-04-05 00:59:54 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connect to mongoDB
+mongoose.connect('mongodb://Ray38:`Kuyue5689740@ds147979.mlab.com:47979/accounts');
+var db = mongoose.connection;
+
+//express:
+//var app = express();
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.json());
+
+app.get('/',function(req, res){
+	res.send('Hello World!');
+
+});
