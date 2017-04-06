e4fbf2e758ebf90425fefffa046c76492d1dd102
Ray <leixx086@umn.edu>
2017-04-05 21:05:54 -0400

    added pet models/Pets.js

---
+++ models/Pets.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+
+//Users Schema
+var petsSchema = mongoose.Schema({
+//	user_name: {
+//		type String,
+//		required: true
+//	},
+	pet_name:{
+		type: String,
+		required: true
+	},
+	species:{
+		type: String,
+		required: true
+	},
+	owner:{
+		type: String,
+		required: true
+	},
+	age:{
+		type: String
+	}
+	create_data:{
+		type: Date,
+		default: Date.now
+	}
+});
+
+var Pets = module.exports = mongoose.model('Pets', petsSchema);
+
+//Get Pets
+module.exports.getPets = function(callback, limit){
+	Pets.find(callback).limit(limit);
+
+}
+
+//Get Pet
+module.exports.getPetById = function(id,callback){
+	Pets.findById(id,callback);
+
+}
