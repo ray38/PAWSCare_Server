2ed8599085bbbf28434c70448f73707feb8c7407
Ray <leixx086@umn.edu>
2017-04-07 01:12:45 -0400

    try to correct

--- client/controllers/pets.js
+++ client/controllers/pets.js
@@ -7,14 +7,30 @@ PAWSCare.controller('PetsController', ['$scope', '$http', '$location', '$routePa
 		//	$scope.pets = response;
 		//});
 	
-		$http.get('/api/pets').then(successCallback, errorCallback);
+		//$http.get('/api/pets').then(successCallback, errorCallback);
 
-		function successCallback(response){
-		    $scope.pets = response;
-		}
-		function errorCallback(error){
+		//function successCallback(response){
+		//    $scope.pets = response;
+		//}
+		//function errorCallback(error){
 		    //error code
+		//    console.log("error http get");
+		//}
+
+		$http({
+		  method: 'GET',
+		  url: '/api/pets'
+		}).then(function successCallback(response) {
+		    // this callback will be called asynchronously
+		    // when the response is available
+		    //console.log('PetsController2 loaded');
+		    $scope.pets=response.data;
+		    console.log($scope.pets);
+
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
 		    console.log("error http get");
-		}
+		  });
 	}
 }]);
