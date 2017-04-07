403e24232fd07234871731e64fefded9160fde9c
Ray <leixx086@umn.edu>
2017-04-06 23:43:31 -0400

    1

--- client/controllers/Pets.js
+++ client/controllers/Pets.js
@@ -3,9 +3,17 @@ var PAWSCare = angular.module('PAWSCare');
 PAWSCare.controller('PetsController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
 	console.log('PetsController loaded');
 	$scope.getPets = function(){
-		$http.get('/api/pets').success(function(response){
-			$scope.pets = response;
-		});
-	}
+		//$http.get('/api/pets').success(function(response){
+		//	$scope.pets = response;
+		//});
+	
+		$http.get('/api/pets').then(successCallback, errorCallback);
 
+		function successCallback(response){
+		    $scope.pets = response;
+		}
+		function errorCallback(error){
+		    //error code
+		}
+	}
 }]);
