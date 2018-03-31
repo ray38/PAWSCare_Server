0f8113f47e2c5db49ac21759c0e8a594318dbb01
ray38 <xlei38@gatech.edu>
2018-03-31 18:56:06 -0400

    test

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
 	'secret': 'RayisAwesomeGraceisALittleSlowTJisQuick',
- 	'database': 'mongodb://Ray38:`Kuyue5689740@ds147979.mlab.com:47979/accounts'
+ 	//'database': 'mongodb://Ray38:`Kuyue5689740@ds147979.mlab.com:47979/accounts'
+	'database': 'mongodb://Admin:PAWSCareisNi$e@ds147979.mlab.com:47979/accounts'
 };
